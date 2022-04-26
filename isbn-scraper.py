3f0080e679358ac710ef91dc84c247f8e77676c2
pgmcr <ph.gilde@gmail.com>
2022-04-26 17:03:09 +0200

    clean up

--- isbn-scraper.py
+++ isbn-scraper.py
@@ -1,21 +1,9 @@
 import json
 import pandas as pd
-from os.path import exists
-import re
-import sys
-
 import requests
 
 
-def validate_input_path(path):
-    if not exists(path):
-        return False
-    if re.search("([^\\s]+(\\.(?i)(csv))$)", path):
-        return True
-    else:
-        return False
-
-
+# get the json containing information on the book from the api
 def call_api(isbn: str):
     url = f"http://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&jscmd=data&format=json"
     result = json.loads(requests.get(url).content)
@@ -27,15 +15,11 @@ def call_api(isbn: str):
 
 
 input_path = input("Input file: ")
-if not validate_input_path(input_path):
-    print("Invalid input path!")
-    sys.exit(0)
-
 output_path = input("Output file: ")
 
 df = pd.read_csv(input_path, names=["ISBN"])
-print(df)
 df["JSON"] = df["ISBN"].apply(call_api)
+
 df["Authors"] = df["JSON"].apply(
     lambda x: "; ".join(author["name"] for author in x["authors"])
 )
