183e33e03b19889efa5aa0381f02719a767b43c7
pgmcr <ph.gilde@gmail.com>
2022-04-27 21:29:57 +0200

    single window

--- isbn-scraper.py
+++ isbn-scraper.py
@@ -1,32 +1,74 @@
 import json
 import pandas as pd
 import requests
-from tkinter import filedialog, Tk, messagebox
+from tkinter import filedialog, Tk, messagebox, ttk, DISABLED, NORMAL
 
 
-# get the json containing information on the book from the api
-def call_api(isbn: str):
-    url = f"http://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&jscmd=data&format=json"
-    result = json.loads(requests.get(url).content)
-    if result:
-        return result[f"ISBN:{isbn}"]
-    else:
-        return False
+class App:
+    def __init__(self):
+        self.root = Tk()
+        self.frame = ttk.Frame(self.root, padding=10)
+
+        self.frame.grid()
+        self.input_file_button = ttk.Button(
+            self.frame, text="Select input file...", command=self.choose_file
+        )
+        self.input_file_button.grid(row=0, column=0)
+
+        self.input_file_label = ttk.Label(self.frame, text="No file selected")
+        self.input_file_label.grid(row=0, column=1)
+
+        self.input_file = None
+
+        self.augment_button = ttk.Button(
+            self.frame,
+            text="Augment!",
+            state=DISABLED,
+            command=self.augment_data,
+        )
+        self.augment_button.grid(row=1, column=0)
+
+        self.save_button = ttk.Button(
+            self.frame, text="Save to file...", state=DISABLED, command=self.save
+        )
+        self.save_button.grid(row=1, column=1)
+
+        self.root.mainloop()
+
+    def choose_file(self):
+        self.input_file = filedialog.askopenfilename(
+            title="Select input file", parent=self.root
+        )
+        self.input_file_label.config(text=self.input_file)
+        self.augment_button.config(state=NORMAL)
+
+    @staticmethod
+    def call_api(isbn):
+        url = f"http://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&jscmd=data&format=json"
+        result = json.loads(requests.get(url).content)
+        if result:
+            return result[f"ISBN:{isbn}"]
+        else:
+            return False
+
+    def augment_data(self):
+        df = pd.read_csv(self.input_file, names=["ISBN"])
+        df["JSON"] = df["ISBN"].apply(App.call_api)
+        df["Authors"] = df["JSON"].apply(
+            lambda x: "; ".join(author["name"] for author in x["authors"]) if x else None
+        )
+        df["Title"] = df["JSON"].apply(lambda x: x["title"] if x else None)
+
+        self.df = df
+
+        self.save_button.config(state=NORMAL)
+        messagebox.showinfo(title="Done!", message="Data successfully augmented!", parent=self.root)
+
+    def save(self):
+        save_path = filedialog.asksaveasfilename(
+            title="Choose output Location", parent=self.root
+        )
+        self.df[["ISBN", "Authors", "Title"]].to_csv(save_path)
 
 
-parent = Tk()
-
-
-input_path = filedialog.askopenfilename(title="Choose input File", parent=parent)
-output_path = filedialog.asksaveasfilename(title="Choose output Location", parent=parent)
-
-df = pd.read_csv(input_path, names=["ISBN"])
-df["JSON"] = df["ISBN"].apply(call_api)
-
-df["Authors"] = df["JSON"].apply(
-    lambda x: "; ".join(author["name"] for author in x["authors"])
-)
-df["Title"] = df["JSON"].apply(lambda x: x["title"])
-print(df[["ISBN", "Title", "Authors"]])
-df[["ISBN", "Title", "Authors"]].to_csv(output_path, errors="ignore")
-messagebox.showinfo("Done", "The file was successfully augmented!", parent=parent)
+App()
